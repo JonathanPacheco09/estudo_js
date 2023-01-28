e6f4b92657a118f3d22b94df4eb5cb2c2d85aee2
jonathan <fjonathanrpacheco09@gmail.com>
2023-01-28 18:17:12 -0300

    atualizado1

--- variaveis/primeiro_codigo.js
+++ variaveis/primeiro_codigo.js
@@ -4,3 +4,9 @@ var idade = 23
 console.log("A idade do usuario é: "+idade)
 var altura = 1.67
 console.log("A altura do usuario é: "+altura)
+var cpf = "xxx-xxx-xxx-xx"
+console.log("O cpf do usuario é: "+cpf)
+var carro ="Dodge ram 2500"
+console.log("O carro do usuario é: "+carro)
+var salario = 8000 
+console.log("O salario do usuario é: "+salario)
