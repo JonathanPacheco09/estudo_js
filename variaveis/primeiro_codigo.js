00a855d190bde3c11fc3c97e20859671eddb91be
jonathan <fjonathanrpacheco09@gmail.com>
2023-01-28 17:36:10 -0300

    primeirocodigo

---
+++ variaveis/primeiro_codigo.js
@@ -0,0 +1,6 @@
+var nome = "Jonathan"
+console.log("o nome do usuario é: "+nome)
+var idade = 23
+console.log("a idade do usuario é: "+idade)
+var altura = 1.67
+console.log("a altura do usuario é: "+altura)
