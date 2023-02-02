d8a8e7bb2c525126c1c6422f3d4189d9f57abff2
jonathan <fjonathanrpacheco09@gmail.com>
2023-02-02 20:51:22 -0300

    operacoessmds

---
+++ variaveis/operacoes.js
@@ -0,0 +1,10 @@
+var a = 5
+var b = 2 
+//soma
+console.log("a soma é: ",a + b)
+//multuplicacao
+console.log("a multuplicacao é: ", a * b)
+//divisao
+console.log("o resultado da divisao é: ", a / b)
+//subtracao
+console.log("o resultado da subtracao é: ", a - b)
