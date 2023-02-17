e21421214075bdc6bf35d607d3f00c5b93bee646
jonathan <fjonathanrpacheco09@gmail.com>
2023-02-16 23:48:51 -0300

    atualizada

---
+++ condicionais/habilitacao.js
@@ -0,0 +1,9 @@
+var idade = 19
+console.log("idade é :" + idade)
+if(idade < 18){
+    console.log("esperar completar 18 anos")
+} else if (idade > 18){
+    console.log("ja pode tirar a habilitacao")
+} if(idade == 18){
+    console.log("ganhou de graca")
+}
